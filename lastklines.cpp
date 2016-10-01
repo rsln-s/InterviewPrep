f6b436111ed597a2ed20f4fcab490937fdf61994
Ruslan Shaydulin <ruslan@l2sh.ru>
2016-10-01 15:53:45 -0400

    version with fifo

--- lastklines.cpp
+++ lastklines.cpp
@@ -2,30 +2,29 @@
 
 #include <iostream>
 #include <fstream>
-#include <algorithm>
+#include <queue>
 #include <iterator>
 #include <string>
-#define BUF_SIZE 256
 #define NUM_LINES 7
 
 int main(int argc, char* argv[]){
 	std::ifstream myfile("linefile.txt");
 	std::string buffer;
-	std::vector <std::string> lines;
+	std::queue <std::string> lines;
 	
-	while(lines.size() < NUM_LINES){
+	while(lines.size() < NUM_LINES+1){
 		std::getline(myfile, buffer);
-		lines.push_back(buffer);
+		lines.push(buffer);
 	}
 	
-	int i = 0;
 	while(!myfile.eof()){
 		std::getline(myfile, buffer);
-		lines.at(i) = buffer;
-		i++;
+		lines.pop();
+		lines.push(buffer);
 	}
-	for (auto it = lines.begin(); it != lines.end(); ++it){
-		std::cout << *it << '\n';
+	while(lines.size() > 1){
+		std::cout << lines.front() << '\n';
+		lines.pop();
 	}
 	return 0;
 }
